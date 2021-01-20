cd978868d9a185d9e15286dd75cba1a134dc76e6
Youssef Mohamed <y.mohmamed@aprcot.com>
2021-01-20 16:07:58 +0200

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by OmarMansour on 1/20/21.
+//  Copyright © 2021 Youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
